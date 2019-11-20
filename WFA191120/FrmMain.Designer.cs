535b7a0e50806edc8d46166cded25d54443b7314
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2019-11-20 21:21:34 +0100

    Add project files.

---
+++ WFA191120/FrmMain.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WFA191120
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbS = new System.Windows.Forms.TextBox();
+            this.btnDoIt = new System.Windows.Forms.Button();
+            this.tbO = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "sor:";
+            // 
+            // tbS
+            // 
+            this.tbS.Location = new System.Drawing.Point(79, 12);
+            this.tbS.Name = "tbS";
+            this.tbS.Size = new System.Drawing.Size(100, 22);
+            this.tbS.TabIndex = 1;
+            this.tbS.Text = "3";
+            // 
+            // btnDoIt
+            // 
+            this.btnDoIt.Location = new System.Drawing.Point(15, 84);
+            this.btnDoIt.Name = "btnDoIt";
+            this.btnDoIt.Size = new System.Drawing.Size(164, 25);
+            this.btnDoIt.TabIndex = 2;
+            this.btnDoIt.Text = "Do It!";
+            this.btnDoIt.UseVisualStyleBackColor = true;
+            this.btnDoIt.Click += new System.EventHandler(this.btnDoIt_Click);
+            // 
+            // tbO
+            // 
+            this.tbO.Location = new System.Drawing.Point(79, 45);
+            this.tbO.Name = "tbO";
+            this.tbO.Size = new System.Drawing.Size(100, 22);
+            this.tbO.TabIndex = 4;
+            this.tbO.Text = "4";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "oszlop:";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(191, 121);
+            this.Controls.Add(this.tbO);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnDoIt);
+            this.Controls.Add(this.tbS);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.Text = "Gombos bisz-basz";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbS;
+        private System.Windows.Forms.Button btnDoIt;
+        private System.Windows.Forms.TextBox tbO;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WFA191120/FrmMain.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA191120
+{
+    public partial class FrmMain : Form
+    {
+        static Random rnd = new Random();
+        //gomb mátrix megosztott változóban
+        Button[,] matrix;
+        public FrmMain()
+        {
+            InitializeComponent();
+            //Form-ra beállítom, hogy méreteződjön a tartalomhoz
+            AutoSize = true;
+
+        }
+
+        private void btnDoIt_Click(object sender, EventArgs e)
+        {
+            //mátrixot inicializálom akkorára, ami a textoxokba van írva
+            matrix = new Button[int.Parse(tbO.Text), int.Parse(tbS.Text)];
+
+            //törlök a formról minden szart:
+            this.Controls.Clear();
+
+            //bejárom a mátroxot:
+            for (int s = 0; s < matrix.GetLength(0); s++)
+            {
+                for (int o = 0; o < matrix.GetLength(1); o++)
+                {
+                    //íme az "egyszerűbb" szintax több tulajdonság beállítására
+                    //figyelj rá, hogy VESSZŐ és nem PONTOSVESSZŐ van a tulajdonságok közt
+                    matrix[s, o] = new Button()
+                    {
+                        Width = 100,
+                        Height = 100,
+                        Location = new Point(s * 100, o * 100),
+                        //3 véletlen szám a színének RGB kódja:
+                        BackColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256)),
+                    };
+
+                    //hozzá kell adni a control-listához (elhagytam a this-t)
+                    Controls.Add(matrix[s, o]);
+
+                    //létrehuk neki egy eseménykezelőt
+                    // += begépelése után tabulátor, és legenerálja a fejrészt
+                    matrix[s, o].Click += GombKattintas;
+                }
+            }
+        }
+
+        private void GombKattintas(object sender, EventArgs e)
+        {
+            //ezt kitöröltem:
+            //throw new NotImplementedException();
+
+            //a paraméterben küldött 'sender' azt a gombot tárolja, amire rákattintottam
+
+            //mivel ez 'csak' object "vissza kell alakítani" gombá, hogy tudjam használni:
+            //mivel a selector (a pont) prió művelet, ezért kell zárójelbe tenni
+            if ((sender as Button).BackColor == Color.Blue)
+            {
+                (sender as Button).BackColor = Color.Red;
+            }
+            else
+            {
+                (sender as Button).BackColor = Color.Blue;
+            }
+        }
+    }
+}
